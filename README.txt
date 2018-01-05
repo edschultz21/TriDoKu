20bd49babff78ca33f9f72270e3acfa8f8edc8bd
edschultz <eschultz@showingtime.com>
2018-01-05 10:09:15 -0600

    Major refactor. Implement hexagons.

--- README.txt
+++ README.txt
@@ -1,15 +1,15 @@
 ﻿
-	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18
+	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20
 0																			
-1										x									
-2									x	x	x								
-3								x	x	x	x	x							
-4							p	y	y	y	y	y	z						
-5						p	p	p	y	y	y	z	z	z					
-6					p	p	p	p	p	y	z	z	z	z	z				
-7				w	q	q	q	q	q	r	s	s	s	s	s	k			
-8			w	w	w	q	q	q	r	r	r	s	s	s	k	k	k		
-9		w	w	w	w	w	q	r	r	r	r	r	s	k	k	k	k	k	
+1											x									
+2										x	x	x								
+3									x	x	x	x	x							
+4								p	y	y	y	y	y	z						
+5							p	p	p	y	y	y	z	z	z					
+6						p	p	p	p	p	y	z	z	z	z	z				
+7					w	q	q	q	q	q	r	s	s	s	s	s	k			
+8				w	w	w	q	q	q	r	r	r	s	s	s	k	k	k		
+9			w	w	w	w	w	q	r	r	r	r	r	s	k	k	k	k	k	
 10																			
 
 Up Triangles:
@@ -17,55 +17,57 @@ Start - x,y
 Cont  - (x-1, x, x+1),y+1
         (x-2, x-1, x, x+1, x+2),y+2
 
-Starts - (9,0) (6,4) (12,4) (3,7) (9,7) (15,7)
+Starts - (10,1) (7,4) (13,4) (4,7) (10,7) (16,7)
 
 Down triangles:
 Start - x,y
 Cont - (x-1, x, x+1),y-1
        (x-2, x-1, x, x+1, x+2),y-2
 
-Starts - (9,6) (6,9) (12,9)
+Starts - (10,6) (7,9) (13,9)
+
+OUTSIDES:
+=========
+NW - 
+Start - 10,1
+Cont - x-1,y+1
+
+NE -
+Start - 10,1
+Cont - x+1,y+1
+
+Bottom -
+Start - 1,10
+Cont - x+2,y
+
+INSIDES:
+========
+Top - 
+Start - 6,5
+Cont - x+1,y
+
+SW - 
+Start - 6,5
+Cont - x,y+1;x+1,y
+eg (5,5) (5,6;6,6) (6,7;7,7) (7,8;8,8) (8,9;9,9)
+
+SE -
+Start - 14,5
+Cont - x,y+1;x-1,y
+eg (13,5) (13,5;12,6) (12,7;11,7) (11,8;10,8) (10,9;9,9)
+
+
 
 Hexagonals:
-Up triangle:
+===========
+Down triangle:
 Start - x,y
 Cont - (x-2, x-1, x, x+1, x+2),y-1
             (x-2, x-1, x+1, x+2),y
             (x-1, x, x+1), y+1
 
-Down triangle:
+Up triangle:
 Start - x,y
 Cont - (x-1, x, x+1),y-1
             (x-2, x-1, x+1, x+2),y
             (x-2, x-1, x, x+1, x+ 2), y+1
-
-
-OUTSIDES:
-NW - 
-Start - 9,1
-Cont - x-1,y+1
-
-NE -
-Start - 9,1
-Cont - x+1,y+1
-
-Bottom -
-Start - 1,9
-Cont - x+2,y
-
-INSIDES:
-Top - 
-Start - 5,5
-Cont - x+1,y
-
-SW - 
-Start - 5,5
-Cont - x,y+1;x+1,y
-eg (5,5) (5,6;6,6) (6,7;7,7) (7,8;8,8) (8,9;9,9)
-
-SE -
-Start - 13,5
-Cont - x,y+1;x-1,y
-eg (13,5) (13,5;12,6) (12,7;11,7) (11,8;10,8) (10,9;9,9)
-
-
--- TriDoKu.cs
+++ TriDoKu.cs
@@ -7,39 +7,323 @@ using System.Threading.Tasks;
 
 namespace Triangles
 {
+    public enum CellSetType
+    {
+        OUTSIDE_NW,
+        OUTSIDE_NE,
+        OUTSIDE_BOT,
+
+        INSIDE_SW,
+        INSIDE_SE,
+        INSIDE_TOP,
+
+        TRIANGLE_1U,
+        TRIANGLE_2U,
+        TRIANGLE_4U,
+        TRIANGLE_5U,
+        TRIANGLE_7U,
+        TRIANGLE_9U,
+
+        TRIANGLE_3D,
+        TRIANGLE_6D,
+        TRIANGLE_8D,
+
+        HEXAGON
+    }
+
+    public class CellSet
+    {
+        public CellSetType CellSetType { get; set; }
+        public int[,] Data { get; set; }
+
+        public CellSet(CellSetType cellSetType)
+        {
+            if (cellSetType == CellSetType.HEXAGON)
+            {
+                Data = new int[12, 2];
+            }
+            else
+            {
+                Data = new int[9, 2];
+            }
+
+            CellSetType = cellSetType;
+
+            switch (CellSetType)
+            {
+                case CellSetType.TRIANGLE_1U:
+                case CellSetType.TRIANGLE_2U:
+                case CellSetType.TRIANGLE_4U:
+                case CellSetType.TRIANGLE_5U:
+                case CellSetType.TRIANGLE_7U:
+                case CellSetType.TRIANGLE_9U:
+                    PopulateTriangleUp();
+                    break;
+
+                case CellSetType.TRIANGLE_3D:
+                case CellSetType.TRIANGLE_6D:
+                case CellSetType.TRIANGLE_8D:
+                    PopulateTriangleDown();
+                    break;
+
+                case CellSetType.OUTSIDE_NW:
+                case CellSetType.OUTSIDE_NE:
+                case CellSetType.OUTSIDE_BOT:
+                    PopulateOutsides();
+                    break;
+
+                case CellSetType.INSIDE_SW:
+                case CellSetType.INSIDE_SE:
+                case CellSetType.INSIDE_TOP:
+                    PopulateInsides();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        #region Populate
+
+        private void PopulateTriangleUp()
+        {
+            int x = 0, y = 0, cell = 0;
+
+            switch (CellSetType)
+            {
+                case CellSetType.TRIANGLE_1U: x = 10; y = 1; break;
+                case CellSetType.TRIANGLE_2U: x = 7; y = 4; break;
+                case CellSetType.TRIANGLE_4U: x = 13; y = 4; break;
+                case CellSetType.TRIANGLE_5U: x = 4; y = 7; break;
+                case CellSetType.TRIANGLE_7U: x = 10; y = 7; break;
+                case CellSetType.TRIANGLE_9U: x = 16; y = 7; break;
+            }
+
+            Data[cell, 0] = x;
+            Data[cell, 1] = y;
+            cell++;
+            for (int i = -1; i <= 1; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y + 1;
+                cell++;
+            }
+            for (int i = -2; i <= 2; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y + 2;
+                cell++;
+            }
+        }
+
+        private void PopulateTriangleDown()
+        {
+            int x = 0, y = 0, cell = 0;
+
+            switch (CellSetType)
+            {
+                case CellSetType.TRIANGLE_3D: x = 10; y = 6; break;
+                case CellSetType.TRIANGLE_6D: x = 7; y = 9; break;
+                case CellSetType.TRIANGLE_8D: x = 13; y = 9; break;
+            }
+
+            Data[cell, 0] = x;
+            Data[cell, 1] = y;
+            cell++;
+            for (int i = -1; i <= 1; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y - 1;
+                cell++;
+            }
+            for (int i = -2; i <= 2; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y - 2;
+                cell++;
+            }
+        }
+
+        private void PopulateOutsides()
+        {
+            switch (CellSetType)
+            {
+                case CellSetType.OUTSIDE_NW:
+                    Data[0, 0] = 10;
+                    Data[0, 1] = 1;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Data[i, 0] = Data[i - 1, 0] - 1;
+                        Data[i, 1] = Data[i - 1, 1] + 1;
+                    }
+                    break;
+                case CellSetType.OUTSIDE_NE:
+                    Data[0, 0] = 10;
+                    Data[0, 1] = 1;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Data[i, 0] = Data[i - 1, 0] + 1;
+                        Data[i, 1] = Data[i - 1, 1] + 1;
+                    }
+                    break;
+                case CellSetType.OUTSIDE_BOT:
+                    Data[0, 0] = 2;
+                    Data[0, 1] = 9;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Data[i, 0] = Data[i - 1, 0] + 2;
+                        Data[i, 1] = Data[i - 1, 1];
+                    }
+                    break;
+            }
+        }
+
+        private void PopulateInsides()
+        {
+            switch (CellSetType)
+            {
+                case CellSetType.INSIDE_SW:
+                    Data[0, 0] = 6;
+                    Data[0, 1] = 5;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Data[i, 0] = Data[i - 1, 0];
+                        Data[i, 1] = Data[i - 1, 1] + 1;
+                        i++;
+                        Data[i, 0] = Data[i - 1, 0] + 1;
+                        Data[i, 1] = Data[i - 1, 1];
+                    }
+                    break;
+                case CellSetType.INSIDE_SE:
+                    Data[0, 0] = 14;
+                    Data[0, 1] = 5;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Data[i, 0] = Data[i - 1, 0];
+                        Data[i, 1] = Data[i - 1, 1] + 1;
+                        i++;
+                        Data[i, 0] = Data[i - 1, 0] - 1;
+                        Data[i, 1] = Data[i - 1, 1];
+                    }
+                    break;
+                case CellSetType.INSIDE_TOP:
+                    Data[0, 0] = 6;
+                    Data[0, 1] = 5;
+                    for (int i = 1; i < 9; i++)
+                    {
+                        Data[i, 0] = Data[i - 1, 0] + 1;
+                        Data[i, 1] = Data[i - 1, 1];
+                    }
+                    break;
+            }
+        }
+
+        public void PopulateHexagon(int x, int y)
+        {
+            var number = x + y;
+            if (number % 2 == 0)
+            {
+                PopulateHexagonDown(x, y);
+            }
+            else
+            {
+                PopulateHexagonUp(x, y);
+            }
+        }
+
+        private void PopulateHexagonUp(int x, int y)
+        {
+            int cell = 0;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y - 1;
+                cell++;
+            }
+            for (int i = -2; i <= 2; i++)
+            {
+                if (i != 0)
+                {
+                    Data[cell, 0] = x + i;
+                    Data[cell, 1] = y;
+                    cell++;
+                }
+            }
+            for (int i = -2; i <= 2; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y + 1;
+                cell++;
+            }
+        }
+
+        private void PopulateHexagonDown(int x, int y)
+        {
+            int cell = 0;
+
+            for (int i = -2; i <= 2; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y - 1;
+                cell++;
+            }
+            for (int i = -2; i <= 2; i++)
+            {
+                if (i != 0)
+                {
+                    Data[cell, 0] = x + i;
+                    Data[cell, 1] = y;
+                    cell++;
+                }
+            }
+            for (int i = -1; i <= 1; i++)
+            {
+                Data[cell, 0] = x + i;
+                Data[cell, 1] = y + 1;
+                cell++;
+            }
+        }
+
+        #endregion
+    }
+
     public class TriDoKu
     {
-        private int[,] _cells = new int[19, 11];
+        private int[,] _cells = new int[21, 11];
         private bool[] _hasNumber = new bool[10];
 
         #region Initialization
 
-        private static int[,] OUTSIDE_NW = new int[,] { { 9, 1 }, { 8, 2 }, { 7, 3 }, { 6, 4 }, { 5, 5 }, { 4, 6 }, { 3, 7 }, { 2, 8 }, { 1, 9 } };
-        private static int[,] OUTSIDE_NE = new int[,] { { 9, 1 }, { 10, 2 }, { 11, 3 }, { 12, 4 }, { 13, 5 }, { 14, 6 }, { 15, 7 }, { 16, 8 }, { 17, 9 } };
-        private static int[,] OUTSIDE_BOT = new int[,] { { 1, 9 }, { 3, 9 }, { 5, 9 }, { 7, 9 }, { 9, 9 }, { 11, 9 }, { 13, 9 }, { 15, 9 }, { 17, 9 } };
-        private static int[,] INSIDE_SW = new int[,] { { 5, 5 }, { 5, 6 }, { 6, 6 }, { 6, 7 }, { 7, 7 }, { 7, 8 }, { 8, 8 }, { 8, 9 }, { 9, 9 } };
-        private static int[,] INSIDE_SE = new int[,] { { 13, 5 }, { 13, 6 }, { 12, 6 }, { 12, 7 }, { 11, 7 }, { 11, 8 }, { 10, 8 }, { 10, 9 }, { 9, 9 } };
-        private static int[,] INSIDE_TOP = new int[,] { { 5, 5 }, { 6, 5 }, { 7, 5 }, { 8, 5 }, { 9, 5 }, { 10, 5 }, { 11, 5 }, { 12, 5 }, { 13, 5 } };
-        private static int[,] TRIANGLE_1U = new int[,] { { 9, 1 }, { 8, 2 }, { 9, 2 }, { 10, 2 }, { 7, 3 }, { 8, 3 }, { 9, 3 }, { 10, 3 }, { 11, 3 } };
-        private static int[,] TRIANGLE_2U = new int[,] { { 6, 4 }, { 5, 5 }, { 6, 5 }, { 7, 5 }, { 4, 6 }, { 5, 6 }, { 6, 6 }, { 7, 6 }, { 8, 6 } };
-        private static int[,] TRIANGLE_3D = new int[,] { { 9, 6 }, { 8, 5 }, { 9, 5 }, { 10, 5 }, { 7, 4 }, { 8, 4 }, { 9, 4 }, { 10, 4 }, { 11, 4 } };
-        private static int[,] TRIANGLE_4U = new int[,] { { 12, 4 }, { 11, 5 }, { 12, 5 }, { 13, 5 }, { 10, 6 }, { 11, 6 }, { 12, 6 }, { 13, 6 }, { 14, 6 } };
-        private static int[,] TRIANGLE_5U = new int[,] { { 3, 7 }, { 2, 8 }, { 3, 8 }, { 4, 8 }, { 1, 9 }, { 2, 9 }, { 3, 9 }, { 4, 9 }, { 5, 9 } };
-        private static int[,] TRIANGLE_6D = new int[,] { { 6, 9 }, { 5, 8 }, { 6, 8 }, { 7, 8 }, { 4, 7 }, { 5, 7 }, { 6, 7 }, { 7, 7 }, { 8, 7 } };
-        private static int[,] TRIANGLE_7U = new int[,] { { 9, 7 }, { 8, 8 }, { 9, 8 }, { 10, 8 }, { 7, 9 }, { 8, 9 }, { 9, 9 }, { 10, 9 }, { 11, 9 } };
-        private static int[,] TRIANGLE_8D = new int[,] { { 12, 9 }, { 11, 8 }, { 12, 8 }, { 13, 8 }, { 10, 7 }, { 11, 7 }, { 12, 7 }, { 13, 7 }, { 14, 7 } };
-        private static int[,] TRIANGLE_9U = new int[,] { { 15, 7 }, { 14, 8 }, { 15, 8 }, { 16, 8 }, { 13, 9 }, { 14, 9 }, { 15, 9 }, { 16, 9 }, { 17, 9 } };
+        private Dictionary<CellSetType, CellSet> _cellSets;
 
-        private readonly int[][,] _triangles = new int[][,] 
+        private void InitializeCellSets()
         {
-            OUTSIDE_NW, OUTSIDE_NE, OUTSIDE_BOT, INSIDE_SW, INSIDE_SE, INSIDE_TOP,
-            TRIANGLE_1U, TRIANGLE_2U, TRIANGLE_3D, TRIANGLE_4U, TRIANGLE_5U, TRIANGLE_6D, TRIANGLE_7U, TRIANGLE_8D, TRIANGLE_9U
-        };
+            _cellSets = new Dictionary<CellSetType, CellSet>();
 
+            _cellSets.Add(CellSetType.OUTSIDE_NW, new CellSet(CellSetType.OUTSIDE_NW));
+            _cellSets.Add(CellSetType.OUTSIDE_NE, new CellSet(CellSetType.OUTSIDE_NE));
+            _cellSets.Add(CellSetType.OUTSIDE_BOT, new CellSet(CellSetType.OUTSIDE_BOT));
+            _cellSets.Add(CellSetType.INSIDE_SW, new CellSet(CellSetType.INSIDE_SW));
+            _cellSets.Add(CellSetType.INSIDE_SE, new CellSet(CellSetType.INSIDE_SE));
+            _cellSets.Add(CellSetType.INSIDE_TOP, new CellSet(CellSetType.INSIDE_TOP));
+            _cellSets.Add(CellSetType.TRIANGLE_1U, new CellSet(CellSetType.TRIANGLE_1U));
+            _cellSets.Add(CellSetType.TRIANGLE_2U, new CellSet(CellSetType.TRIANGLE_2U));
+            _cellSets.Add(CellSetType.TRIANGLE_3D, new CellSet(CellSetType.TRIANGLE_3D));
+            _cellSets.Add(CellSetType.TRIANGLE_4U, new CellSet(CellSetType.TRIANGLE_4U));
+            _cellSets.Add(CellSetType.TRIANGLE_5U, new CellSet(CellSetType.TRIANGLE_5U));
+            _cellSets.Add(CellSetType.TRIANGLE_6D, new CellSet(CellSetType.TRIANGLE_6D));
+            _cellSets.Add(CellSetType.TRIANGLE_7U, new CellSet(CellSetType.TRIANGLE_7U));
+            _cellSets.Add(CellSetType.TRIANGLE_8D, new CellSet(CellSetType.TRIANGLE_8D));
+            _cellSets.Add(CellSetType.TRIANGLE_9U, new CellSet(CellSetType.TRIANGLE_9U));
+            _cellSets.Add(CellSetType.HEXAGON, new CellSet(CellSetType.HEXAGON));
+        }
         #endregion
 
         public void Run()
         {
+            InitializeCellSets();
+
             // Triangles1 - Good
             // Triangles2 - OUTSIDE_NW, OUTSIDE_NE, TRIANGLE_1U
             // Triangles3 - OUTSIDE_BOT, INSIDE_SW, INSIDE_SE, TRIANGLE_7U
@@ -47,42 +331,25 @@ namespace Triangles
             // Triangles5 - INSIDE_SW, INSIDE_SE, TRIANGLE_2U, TRIANGLE_4U, TRIANGLE_5U, TRIANGLE_6D, TRIANGLE_8D, TRIANGLE_9U
             Read("Triangles1.txt");
 
+            var ezs = ToString();
+
             Validate();
 
             // EZSTODO - Validate hexagons.
             // EZSTODO - Implement logic for solving.
-            // EZSTODO - Add to git.
-            var ezs = ToString();
+
         }
 
         #region Startup
 
-        private void ResetHasNumber()
-        {
-            for (int i = 0; i < _hasNumber.Length; i++)
-            {
-                _hasNumber[i] = false;
-            }
-        }
-
-        private string NumSpaces(int count)
-        {
-            var spaces = "";
-            for (int i = 0; i < count; i++)
-            {
-                spaces += " ";
-            }
-            return spaces;
-        }
-
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            int x = 9, y = 1, length = 1;
+            int x = 10, y = 1, length = 1;
 
             for (int i = 0; i < 9; i++)
             {
-                sb.Append(NumSpaces(9 - i));
+                sb.Append(NumSpaces(10 - i));
 
                 for (int j = 0; j < length; j++)
                 {
@@ -99,15 +366,49 @@ namespace Triangles
             return sb.ToString();
         }
 
+        private string NumSpaces(int count)
+        {
+            var spaces = "";
+            for (int i = 0; i < count; i++)
+            {
+                spaces += " ";
+            }
+            return spaces;
+        }
+
+        private void ResetHasNumber()
+        {
+            for (int i = 0; i < _hasNumber.Length; i++)
+            {
+                _hasNumber[i] = false;
+            }
+        }
+
         private void ResetCells()
         {
-            for (int x = 0; x < 18; x++)
+            int x, y;
+
+            for (x = 0; x < 18; x++)
             {
-                for (int y = 0; y < 10; y++)
+                for (y = 0; y < 10; y++)
                 {
-                    _cells[x, y] = 0;
+                    _cells[x, y] = -1;
                 }
             }
+
+            x = 10;
+            y = 1;
+            var count = 1;
+            do
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    _cells[x + i, y] = 0;
+                }
+                x--;
+                y++;
+                count += 2;
+            } while (y != 10);
         }
 
         private int[] GetValuesFromInput(string line)
@@ -125,7 +426,7 @@ namespace Triangles
 
         private void Read(string filename)
         {
-            int x = 9, y = 1;
+            int x = 10, y = 1;
 
             var lines = File.ReadAllLines(filename);
 
@@ -137,30 +438,78 @@ namespace Triangles
 
         #endregion
 
+        #region Validation
+
         private void Validate()
         {
-            foreach (var triangle in _triangles)
+            foreach (CellSetType cellSetType in Enum.GetValues(typeof(CellSetType)))
             {
-                Validate(triangle);
+                if (cellSetType != CellSetType.HEXAGON)
+                {
+                    Validate(cellSetType);
+                }
             }
+
+            ValidateHexagons();
         }
 
-        private void Validate(int[,] triangle)
+        private void Validate(CellSetType cellSetType)
+        {
+            Validate(_cellSets[cellSetType].Data);
+        }
+
+        private void Validate(int[,] cellSet)
         {
             ResetHasNumber();
 
             for (int i = 0; i < 9; i++)
             {
-                var x = triangle[i, 0];
-                var y = triangle[i, 1];
+                var x = cellSet[i, 0];
+                var y = cellSet[i, 1];
                 var number = _cells[x, y];
 
-                if (_hasNumber[number])
+                if (number != -1 && number != 0)
+                {
+                    if (_hasNumber[number])
+                    {
+
+                    }
+                    _hasNumber[number] = true;
+                }
+            }
+        }
+
+        private void ValidateHexagon(int[,] cellSet, int centerOfHexagon)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                var x = cellSet[i, 0];
+                var y = cellSet[i, 1];
+                var number = _cells[x, y];
+
+                if (number == centerOfHexagon)
                 {
 
                 }
-                _hasNumber[number] = true;
             }
         }
+
+        private void ValidateHexagons()
+        {
+            int x = 10, y = 1, count = 1;
+            do
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    _cellSets[CellSetType.HEXAGON].PopulateHexagon(x + i, y);
+                    ValidateHexagon(_cellSets[CellSetType.HEXAGON].Data, _cells[x + i, y]);
+                }
+                x--;
+                y++;
+                count += 2;
+            } while (y != 10);
+        }
+
+        #endregion
     }
 }
